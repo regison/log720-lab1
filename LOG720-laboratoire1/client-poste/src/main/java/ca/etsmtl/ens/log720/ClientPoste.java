5e45f44359879305e987ab05859c1c46f14b598e
mrtim <charly.simon.cs@gmail.com>
2016-10-08 17:36:33 -0400

    add terminal librairy

---
+++ LOG720-laboratoire1/client-poste/src/main/java/ca/etsmtl/ens/log720/ClientPoste.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package ca.etsmtl.ens.log720;
+
+/**
+ * @author charly
+ *
+ */
+public class ClientPoste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private static void launchTerminal(){
+		Menu mainMenu = buildMenus();
+		
+	}
+
+	private static Menu buildMenus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
