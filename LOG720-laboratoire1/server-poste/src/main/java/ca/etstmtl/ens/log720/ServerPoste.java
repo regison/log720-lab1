25d5addb6026346b035ebe5c015e32100f41beb0
Reginald Augustin <reginald.augustin@gmail.com>
2016-10-10 15:28:46 -0400

    start poste serveur

--- LOG720-laboratoire1/server-poste/src/main/java/ca/etstmtl/ens/log720/ServerPoste.java
+++ LOG720-laboratoire1/server-poste/src/main/java/ca/etstmtl/ens/log720/ServerPoste.java
@@ -1,9 +1,15 @@
 import org.omg.CORBA.ORBPackage.InvalidName;
 import org.omg.CosNaming.NamingContextExt;
 import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.PortableServer.POA;
 
 public class ServerPoste {
 	private org.omg.CORBA.ORB orb;
+	private org.omg.PortableServer.POA poa;
+	private org.omg.PortableServer.Servant servant_dos;
+	private org.omg.PortableServer.Servant servant_inf;
+	private org.omg.CORBA.Object obj_dos;
+	private org.omg.CORBA.Object obj_inf;
 	private NamingContextExt nc;
 	
 	public ServerPoste(String[] args) throws InvalidName{
@@ -13,7 +19,16 @@ public class ServerPoste {
 		this.nc = NamingContextExtHelper.narrow(orb
 				.resolve_initial_references("NameService"));
 	}
+	
+	public POA getPOA(){
+		return poa;
+	}
+	public NamingContextExt getNameContextExt(){
+		return nc;
+	}
 	public static void main(String [] args){
+		
+		//Initialize POA and all the servant needed
 		
 	}
 }
